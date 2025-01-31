078195a0f6140cb77bc397867e6c509876910fe8
Bruno Rafael <bruno_rafael_08@hotmail.com>
2025-01-31 06:55:23 -0300

    created componet card, card purple, card button, card purple button

--- components/src/app/app.module.ts
+++ components/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardPurpleComponent } from './card-purple/card-purple.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardPurpleButtonComponent } from './card-purple-button/card-purple-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardPurpleComponent,
+    CardButtonComponent,
+    CardPurpleButtonComponent
   ],
   imports: [
     BrowserModule,
